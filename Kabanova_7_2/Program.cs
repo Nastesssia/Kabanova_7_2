b311fee74bf474fe8e7648388401d812fa7e464d
alexa <alexa@DESKTOP-RV4BEBB>
2021-12-16 10:16:02 +0200

    Добавьте файлы проекта.

---
+++ Kabanova_7_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kabanova_7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string str = Console.ReadLine();
+            int k = 0;
+            while (str.IndexOf("*", k) != -1)
+            {
+                k = str.IndexOf("*", k);
+                str = str.Insert(k + 1, " ");
+                k += 1;
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
